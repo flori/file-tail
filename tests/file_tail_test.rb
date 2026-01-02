4f03360d3c584cc7c8b5209a096f73aeb2aa97d2
Florian Frank <flori@ping.de>
2026-01-02 01:49:11 +0100

    Update test file path to use 'tmp' directory
    - Changed the test file path from using `__dir__` to using `'tmp'`
      directory for better test organization

--- tests/file_tail_test.rb
+++ tests/file_tail_test.rb
@@ -12,7 +12,7 @@ class FileTailTest < Test::Unit::TestCase
   include FileUtils
 
   def setup
-    @out = File.new(File.join(__dir__, "test.#$$"), "wb")
+    @out = File.new(File.join('tmp', "test.#$$"), "wb")
     at_exit { path = @out&.path and rm_f File.expand_path(path) }
     append(@out, 100)
     @in = File.new(@out.path, "rb")
